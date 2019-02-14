989bda69a1d0fb25d875972335656b5e7adc2dc5
Henry <yuhaoran@huobi.com>
2019-02-14 16:36:27 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -82,21 +82,22 @@ parse_args() {
 check_version_lang() {
   br=`git branch | grep "*"`
   branch=${br/* /}
+  echo "branch="$branch""
   cn_result=$(echo $branch | grep "cn")
 #   en_result=$(echo $branch | grep "en")
   if [[ "$cn_result" != "" ]]
   then
-    language="cn"
+    language=cn
   else
-    language="en"
+    language=en
   fi
 
   v1_result=$(echo $branch | grep "v1")
   if [[ "$v1_result" != "" ]]
   then
-    version="1"
+    version=1
   else
-    version="2"
+    version=2
   fi
   echo "language="$language""
   echo "version="$version""
